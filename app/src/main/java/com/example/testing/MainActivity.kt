a2a9ebb54406206c3305c3de9f74c7a340edde89
Koosh <kooshgupta1803@gmail.com>
2024-06-17 16:42:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/testing/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.safecamp
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.testing.R
+import com.example.testing.SignUp
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        window.statusBarColor = Color.TRANSPARENT
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, SignUp::class.java))
+            finish()
+        }, 3000)
+
+    }
+
+
+}
